9a351cb53b2906dc6070ebe13cdd5525df88e070
Mikhail Yakhyayeu <m.yakhyayeu@godeltech.com>
2023-10-20 16:35:36 +0200

    Navigation and cleanup

---
+++ green-fin-tech/src/About/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Information about the application...</p>
+    </div>
+  );
+}
+
+export default About;
--- green-fin-tech/src/Navbar/Navbar.tsx
+++ green-fin-tech/src/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom'; // Import the Link component
 
 const Navbar: React.FC = () => {
   return (
@@ -8,9 +9,9 @@ const Navbar: React.FC = () => {
         <Typography variant="h6" style={{ flexGrow: 1 }}>
           Le punir app
         </Typography>
-        <Button color="inherit">Home</Button>
-        <Button color="inherit">About</Button>
-        <Button color="inherit">Contact</Button>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/about">About</Button>
+        <Button color="inherit" component={Link} to="/contacts">Contacts</Button>
       </Toolbar>
     </AppBar>
   );
