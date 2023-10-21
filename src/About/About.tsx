c3221c24f1f56f31452b097ea43e9080065f3233
Mikhail Yakhyayeu <m.yakhyayeu@godeltech.com>
2023-10-21 22:24:33 +0200

    Learn page content

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import { Container, Typography, Paper, Box, ThemeProvider } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 const About: React.FC = () => {
+
+  const theme = useTheme();
+
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>Information about the application...</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Box sx={{ width: '100%', display: 'block' }}>
+          <img src="/images/Cartoon.png" alt="Cartoon" style={{ width: '100%' }} />
+        </Box>
+        
+        <Paper elevation={3} sx={{ margin: '20px 0', padding: '15px', color: theme.palette.secondary.contrastText }}>
+          <Typography variant="h5" gutterBottom>
+            Environmentally Friendly Companies
+          </Typography>
+          <Typography variant="body1">
+            Green companies are not only good for the environment but also for investors. As the world focuses more on sustainability, these companies have great potential.
+          </Typography>
+        </Paper>
+
+        <Paper elevation={3} sx={{ margin: '20px 0', padding: '15px', color: theme.palette.secondary.contrastText }}>
+          <Typography variant="h5" gutterBottom>
+            Discover Stocks with Our App
+          </Typography>
+          <Typography variant="body1">
+            Our app offers an easy way to find stocks on the market with a user-friendly design and real-time data.
+          </Typography>
+        </Paper>
+
+        <Paper elevation={3} sx={{ margin: '20px 0', padding: '15px', color: theme.palette.secondary.contrastText }}>
+          <Typography variant="h5" gutterBottom>
+            AI-Powered Suggestions
+          </Typography>
+          <Typography variant="body1">
+            Our platform uses AI to suggest stocks based on users' past choices, offering a more personalized experience.
+          </Typography>
+        </Paper>
+      </Container>
+    </ThemeProvider>
   );
 }
 
