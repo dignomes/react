e92cf573102c50472c3df0f1c468ec754c6a6288
SilichStudent <32966956+SilichStudent@users.noreply.github.com>
2023-10-21 19:55:09 +0200

    Fake real api (#5)
    * smooth transition
    
    * use real data fake api
    
    ---------
    
    Co-authored-by: sergey.silich <sergey.silich@comparethemarket.com>

--- src/Components/ShareCard/ShareCard.tsx
+++ src/Components/ShareCard/ShareCard.tsx
@@ -1,27 +1,34 @@
 import React from 'react'
 import TinderCard from 'react-tinder-card'
 import {Card} from '@mui/material'
-import {removeItem} from './../../Store/SwipingSlice';
+import {removeItem, sendStockLike, sendStockDislike} from './../../Store/SwipingSlice';
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch, RootState} from "../../Store/Store";
 
-const ShareCard = ({ ticker, image_src, description } : {ticker: string, image_src: string, description: string}) => {
-    // const index = useSelector((state: RootState) => state.swiping.index);
+const ShareCard = ({ id, image_src, description } : {id: number, image_src: string, description: string}) => {
     const dispatch = useDispatch<AppDispatch>();
-    const items = useSelector((state: RootState) => state.swiping.stocks);
 
     const removeCard = () => {
-        dispatch(removeItem(ticker));
+        dispatch(removeItem(id));
+    }
+
+    const handleSwipe = (direction: any) => {
+        if(direction === 'right'){
+            dispatch(sendStockLike(id))
+        } else {
+            dispatch(sendStockDislike(id))
+        }
     }
 
     let isVisible = true
     return (<TinderCard
             className='swipe'
-            onSwipe={(direction) => console.log(`You swiped: ${direction}, index: ${ticker}`)}
+            onSwipe={handleSwipe}
             onCardLeftScreen={removeCard}
             preventSwipe={['up', 'down']}
+            swipeRequirementType='position'
         >
-            <Card style={{backgroundColor: '#f5f5f5', minWidth: '100%', minHeight: '100%'}}>
+            <Card style={{backgroundColor: '#f5f5f5', display: 'flex', justifyContent: 'center', alignItems: 'center', minWidth: '100%', minHeight: '100%'}}>
                 <img src={image_src} className='card' alt='something'/>
             </Card>
             <div className="textOverlay">
