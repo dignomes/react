1985b5d78e433aa06d55f1217cc59bc012058b30
sergey.silich <sergey.silich@comparethemarket.com>
2023-10-22 02:15:27 +0200

    use swippable

--- src/Components/ShareCard/ShareCard.tsx
+++ src/Components/ShareCard/ShareCard.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, {useMemo, useRef} from 'react'
 import TinderCard from 'react-tinder-card'
+import {useSwipeable} from 'react-swipeable'
 import {Card} from '@mui/material'
 import {removeItem, sendStockLike, sendStockDislike} from './../../Store/SwipingSlice';
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch, RootState} from "../../Store/Store";
 
+import Draggable from 'react-draggable'
+
+//
+// const refs = [
+//     React.createRef<any>(),
+//     React.createRef<any>(),
+//     React.createRef<any>(),
+//     React.createRef<any>(),
+//     React.createRef<any>()
+// ]
+
 const ShareCard = () => {
     const dispatch = useDispatch<AppDispatch>();
-    let items = useSelector((state: RootState) => state.swiping.stocks);
-    const current = useSelector((state: RootState) => state.swiping.currentStock);
+    const swiping = useSelector((state: RootState) => state.swiping);
+    const items = swiping.stocks;
+    const current = swiping.currentStock;
 
-    items = [...items].reverse();
+
+    const filtered_items = [...items].reverse();
+    const handlers_arr = filtered_items.map((e,i) => {
+
+    })
+    const handlers = useSwipeable({
+        onSwiped: (eventData) => {
+            console.log("User Swiped!", eventData);
+            dispatch(removeItem(current ? current.id : 0));
+        },
+    });
 
     if (!current) {
         return (<div>no data</div>)
@@ -20,30 +43,33 @@ const ShareCard = () => {
     //     dispatch(removeItem(current.id));
     // }
 
-    const handleSwipe = (direction: any) => {
-        dispatch(removeItem(current.id));
-        if (direction === 'right') {
-            dispatch(sendStockLike(current.id))
-        } else {
-            dispatch(sendStockDislike(current.id))
-        }
+    const handleSwipe = (direction: any, i: number) => {
+        // dispatch(removeItem(current.id));
+        console.log('swipe', i)
+        // if (direction === 'right') {
+        //     dispatch(sendStockLike(current.id))
+        // } else {
+        //     dispatch(sendStockDislike(current.id))
+        // }
     }
 
     console.log(items);
 
+
+    // const swipe = async (dir) => {
+    //     if (canSwipe && currentIndex < db.length) {
+    //         await childRefs[currentIndex].current.swipe(dir) // Swipe the card!
+    //     }
+    // }
+
     let isVisible = true
     return (<Card className='swipe'>
         <div className='tinderCard'>
-            {items.map((r, i) => (<TinderCard
-                onSwipe={handleSwipe}
-                // onCardLeftScreen={removeCard}
-                preventSwipe={['up', 'down']}
-                swipeRequirementType='position'
-                className='cardItem'
-                key={i}
-            >
-                <img src={r.image_url} className='card' alt={r.title}/>
-            </TinderCard>))}
+            {filtered_items.map((r, i) => (<div className='cardItem' key={r.title} {...handlers}>
+                    <Draggable>
+                        <img src={r.image_url} className='card' alt={r.title}/>
+                    </Draggable>
+                </div>))}
         </div>
         <div className="textOverlay">
             {current.description}
@@ -52,3 +78,26 @@ const ShareCard = () => {
 }
 
 export default ShareCard;
+
+
+//
+//             {filtered_items.map((r, i) => (<TinderCard
+//                 // ref={refs[i]}
+//                 onSwipe={(d) => handleSwipe(d, i)}
+//                 onCardLeftScreen={() => console.log('left screen')}
+//                 preventSwipe={['up', 'down']}
+//                 swipeRequirementType='position'
+//                 className='cardItem'
+//                 key={i}
+//             >
+//                 <img src={r.image_url} className='card' alt={r.title}/>
+//             </TinderCard>))}
+
+
+// {
+//     filtered_items.map((r, i) => (
+//         <div className='cardItem' key={r.title}>
+//             <img src={r.image_url} className='card' alt={r.title}/>
+//         </div>
+//     ))
+// }
