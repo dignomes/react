fbef45e2d3ac0b5cb3e9f5f457b5f4536a6a4f30
sergey.silich <sergey.silich@comparethemarket.com>
2023-10-22 02:40:12 +0200

    fix issues

--- src/Components/ShareCard/ShareCard.tsx
+++ src/Components/ShareCard/ShareCard.tsx
@@ -1,5 +1,4 @@
 import React, {useMemo, useRef} from 'react'
-import TinderCard from 'react-tinder-card'
 import {useSwipeable} from 'react-swipeable'
 import {Card} from '@mui/material'
 import {removeItem, sendStockLike, sendStockDislike} from './../../Store/SwipingSlice';
@@ -8,14 +7,6 @@ import {AppDispatch, RootState} from "../../Store/Store";
 
 import Draggable from 'react-draggable'
 
-//
-// const refs = [
-//     React.createRef<any>(),
-//     React.createRef<any>(),
-//     React.createRef<any>(),
-//     React.createRef<any>(),
-//     React.createRef<any>()
-// ]
 
 const ShareCard = () => {
     const dispatch = useDispatch<AppDispatch>();
@@ -24,52 +15,38 @@ const ShareCard = () => {
     const current = swiping.currentStock;
 
 
-    const filtered_items = [...items].reverse();
-    const handlers_arr = filtered_items.map((e,i) => {
-
-    })
+    let filtered_items = [...items].reverse();
+    filtered_items = [...filtered_items].slice(-3);
     const handlers = useSwipeable({
         onSwiped: (eventData) => {
             console.log("User Swiped!", eventData);
-            dispatch(removeItem(current ? current.id : 0));
+            // dispatch(removeItem(current ? current.id : 0));
+            if (eventData.dir === 'Left') {
+                dispatch(sendStockDislike(current ? current.id : 0))
+            }
+            if (eventData.dir === 'Right') {
+                dispatch(sendStockLike(current ? current.id : 0))
+            }
         },
     });
 
     if (!current) {
         return (<div>no data</div>)
     }
-    //
-    // const removeCard = () => {
-    //     dispatch(removeItem(current.id));
-    // }
 
-    const handleSwipe = (direction: any, i: number) => {
-        // dispatch(removeItem(current.id));
-        console.log('swipe', i)
-        // if (direction === 'right') {
-        //     dispatch(sendStockLike(current.id))
-        // } else {
-        //     dispatch(sendStockDislike(current.id))
-        // }
-    }
-
-    console.log(items);
-
-
-    // const swipe = async (dir) => {
-    //     if (canSwipe && currentIndex < db.length) {
-    //         await childRefs[currentIndex].current.swipe(dir) // Swipe the card!
-    //     }
-    // }
-
-    let isVisible = true
     return (<Card className='swipe'>
         <div className='tinderCard'>
-            {filtered_items.map((r, i) => (<div className='cardItem' key={r.title} {...handlers}>
+            {filtered_items.map((r, i) => {
+                return i === filtered_items.length - 1 ? (<div className="cardItem" {...handlers} key={r.title}>
                     <Draggable>
-                        <img src={r.image_url} className='card' alt={r.title}/>
+                        <img src={r.image_url} className="card" alt={r.title}/>
                     </Draggable>
-                </div>))}
+                </div>) : (<div className="cardItem" key={r.title}>
+                    <Draggable>
+                        <img src={r.image_url} className="card" alt={r.title}/>
+                    </Draggable>
+                </div>)
+            })}
         </div>
         <div className="textOverlay">
             {current.description}
@@ -78,26 +55,3 @@ const ShareCard = () => {
 }
 
 export default ShareCard;
-
-
-//
-//             {filtered_items.map((r, i) => (<TinderCard
-//                 // ref={refs[i]}
-//                 onSwipe={(d) => handleSwipe(d, i)}
-//                 onCardLeftScreen={() => console.log('left screen')}
-//                 preventSwipe={['up', 'down']}
-//                 swipeRequirementType='position'
-//                 className='cardItem'
-//                 key={i}
-//             >
-//                 <img src={r.image_url} className='card' alt={r.title}/>
-//             </TinderCard>))}
-
-
-// {
-//     filtered_items.map((r, i) => (
-//         <div className='cardItem' key={r.title}>
-//             <img src={r.image_url} className='card' alt={r.title}/>
-//         </div>
-//     ))
-// }
